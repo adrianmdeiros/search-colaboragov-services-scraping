e4070801c68761b95d160cfa7b14432066bb079b
Adrian L. Medeiros <lima.adrian13@gmail.com>
2024-08-16 11:31:41 +0000

    refactor: avoid code repetitions

--- server.js
+++ server.js
@@ -10,23 +10,33 @@ const agent = new https.Agent({
 
 app.use(express.json())
 
-app.post('/api/search', async (req, res) => {
-    const userMessage = req.body.message
+const baseUrl = 'https://catalogo-colaboragov.sistema.gov.br'
 
-    const search = encodeURIComponent(userMessage).replace(/%20/g, "+")
+const createSearchUrl = (body, separator) => {
+    const searchQuery = encodeURIComponent(body).replace(/%20/g, separator).toLowerCase()
+    const searchUrl = `${baseUrl}/resultado?search=${searchQuery}`
+    return searchUrl
+}
 
-    const baseUrl = 'https://catalogo-colaboragov.sistema.gov.br'
-    const searchUrl = `${baseUrl}/resultado?search=${search}`
+const getHtmlFromPage = async (url) => {
+    const response = await axios.get(url, { httpsAgent: agent })
+    const html = response.data
+    return html
+}
 
-    try {
-        const response = await axios.get(searchUrl, { httpsAgent: agent })
-        const html = response.data
-        
-        const $ = load(html)
+const loadHtml = (html) => {
+    const $ = load(html)
+    return $
+}
 
-        const services = []
-        const lastUl = $('ul').last()
-        lastUl.find('a').each((index, element) => {
+const isLinkPartOfBreadcrumb = ($, aElement) => {
+    return $(aElement).closest('.breadcrumb').length
+}
+
+const createServicesList = ($) => {
+    const services = []
+    $('a').each((index, element) => {
+        if (!isLinkPartOfBreadcrumb($, element)) {
             const link = $(element).attr('href');
             const name = $(element).text()
             const service = {
@@ -34,15 +44,37 @@ app.post('/api/search', async (req, res) => {
                 link,
             }
             services.push(service);
-        });
+        }
+    })
+    return services
+}
+
+app.post('/api/search', async (req, res) => {
+    const userMessage = req.body.message
+
+    const searchUrl = createSearchUrl(userMessage, "+")
+
+    try {
+        const html = await getHtmlFromPage(searchUrl)
+
+        const $ = loadHtml(html)
+
+        const services = createServicesList($)
 
         return res.json({ services })
-
     } catch (error) {
-        console.error('Erro ao buscar dados');
+        console.error(error.message);
         return res.status(500).json({ error: 'Erro ao buscar dados' })
     }
 })
+
+app.post('/api/profile/services', async (req, res) => {
+    
+})
+
+
+
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
