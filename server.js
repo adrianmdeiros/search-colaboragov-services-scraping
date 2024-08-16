306c4ef8c00287b4d9573d8938248ea0833eacfc
Adrian L. Medeiros <lima.adrian13@gmail.com>
2024-08-16 13:20:55 +0000

    feat: add endpoints for all services by profile and category

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json())
 
 const baseUrl = 'https://catalogo-colaboragov.sistema.gov.br'
 
-const createSearchUrl = (body, separator) => {
-    const searchQuery = encodeURIComponent(body).replace(/%20/g, separator).toLowerCase()
-    const searchUrl = `${baseUrl}/resultado?search=${searchQuery}`
+const createSearchUrl = (bodyReq, endpoint, separator) => {
+    const searchQueryOrParams = encodeURIComponent(bodyReq).replace(/%20/g, separator).toLowerCase()
+    const searchUrl = `${baseUrl}${endpoint}${searchQueryOrParams}`
     return searchUrl
 }
 
@@ -49,18 +49,22 @@ const createServicesList = ($) => {
     return services
 }
 
+const useServices = async (searchUrl) => {
+    const html = await getHtmlFromPage(searchUrl)
+    const $ = loadHtml(html)
+    const services = createServicesList($)
+    return { $, services }
+}
+
 app.post('/api/search', async (req, res) => {
     const userMessage = req.body.message
+    const endpoint = "/resultado?search="
+    const separator = "+"
 
-    const searchUrl = createSearchUrl(userMessage, "+")
+    const searchUrl = createSearchUrl(userMessage, endpoint, separator)
 
     try {
-        const html = await getHtmlFromPage(searchUrl)
-
-        const $ = loadHtml(html)
-
-        const services = createServicesList($)
-
+        const { services } = await useServices(searchUrl)
         return res.json({ services })
     } catch (error) {
         console.error(error.message);
@@ -69,11 +73,49 @@ app.post('/api/search', async (req, res) => {
 })
 
 app.post('/api/profile/services', async (req, res) => {
-    
+    const selectedProfile = req.body.profile
+    const endpoint = "/perfil/"
+    const separator = "-"
+    const searchUrl = createSearchUrl(selectedProfile, endpoint, separator)
+
+    try {
+        const { services } = await useServices(searchUrl)
+        return res.json({ services })
+    } catch (error) {
+        console.error(error.message)
+        return res.status(500).json({ error: 'Erro ao buscar dados' })        
+    }
 })
 
+app.post('/api/category/services', async (req, res) => {
+    const selectedCategory = req.body.category
+    const endpoint = "/categoria/"
+    const separator = "-"
+    const searchUrl = createSearchUrl(selectedCategory, endpoint, separator)
 
+    try {
+        const { $ } = await useServices(searchUrl)
+        const services = []
+        $('.tab-content').each((index, tabContent) => {
+            $(tabContent).find('a').each((index, element) => {
+                if (!isLinkPartOfBreadcrumb($, element)) {
+                    const link = $(element).attr('href');
+                    const name = $(element).find('span').text()
+                    const service = {
+                        name,
+                        link,
+                    }
+                   services.push(service)
+                }
+            })
+        })
 
+        return res.json({ services })
+    } catch (error) {
+        console.error(error.message)
+        return res.status(500).json({ error: 'Erro ao buscar dados' })        
+    }
+})
 
 
 const PORT = process.env.PORT || 3000
